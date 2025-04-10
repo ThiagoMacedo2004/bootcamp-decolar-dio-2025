292a9697ff97bf5517b528bacf06d973f380e3d1
ThiagoMacedo2004 <tsouzamacedo@gmail.com>
2025-04-10 01:04:15 -0300

    @ComponentScan(value = {"dio.java.service"})

--- src/main/java/dio/java/Application.java
+++ src/main/java/dio/java/Application.java
@@ -2,11 +2,15 @@ package dio.java;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 
+@EntityScan("dio.java.domain.model")
+@EnableJpaRepositories("dio.java.domain.repository")
+@SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
