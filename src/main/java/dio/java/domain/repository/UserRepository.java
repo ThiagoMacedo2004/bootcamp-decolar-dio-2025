f4518b5177ddd66c63ab5ba4a388a6067b59b3c0
ThiagoMacedo2004 <tsouzamacedo@gmail.com>
2025-04-10 09:10:00 -0300

    Ajustando...

--- src/main/java/dio/java/domain/repository/UserRepository.java
+++ src/main/java/dio/java/domain/repository/UserRepository.java
@@ -4,7 +4,7 @@ import dio.java.domain.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
     boolean existsByAccountNumber(String accountNumber);
